6dded4ee5b883e7a18fa1649f61c4dbde96a38ca
ruanoliver <ruanoliveira862@gmail.com>
2017-09-19 16:54:25 -0300

    teste request

---
+++ testeRequestTrello.js
@@ -0,0 +1,10 @@
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api.trello.com/1/boards/NoBCnvfL/lists?fields=name%2Curl&key=a15c445a9a0bc0efd2332aac48bd1bfc&token=01e8f3762fa91f254aae825f349abb1ec6adb1c8fadb7f03b9d28e3e62127584",
+    "method": "GET"
+  }
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
