9aae7d3e267149b797cbdca84c70c64d8f21ada7
ruanoliver <ruanoliveira862@gmail.com>
2017-09-20 15:09:07 -0300

    inclementei o index

--- testeRequestTrello.js
+++ testeRequestTrello.js
@@ -1,7 +1,7 @@
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://api.trello.com/1/boards/NoBCnvfL/lists?fields=name%2Curl&key=a15c445a9a0bc0efd2332aac48bd1bfc&token=01e8f3762fa91f254aae825f349abb1ec6adb1c8fadb7f03b9d28e3e62127584",
+    "url": "https://api.trello.com/1/boards/SsTUzo2K/lists?fields=name%2Curl&key=a15c445a9a0bc0efd2332aac48bd1bfc&token=01e8f3762fa91f254aae825f349abb1ec6adb1c8fadb7f03b9d28e3e62127584",
     "method": "GET"
   }
   
